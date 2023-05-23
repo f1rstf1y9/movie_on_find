93e3120ddddf68a5d2df5e34e9bc6f8f8dc1dd8b
unknown <tgx963@naver.com>
2023-05-23 23:20:24 +0900

    23/05/23

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,44 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        
-class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = get_user_model()
-        fields = ('first_name', 'last_name',)
+from .models import User
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ('email', 'profile_image', 'nickname', 'kakao_id', 'like_genres')
+
+        widgets={
+            'nickname': forms.Textarea(attrs={"rows":"1"}),
+            'like_genres' : forms.CheckboxSelectMultiple()
+        }
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        user.like_genres.set(self.cleaned_data["like_genres"])
+        return user
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+    email = forms.CharField(disabled=True)
+    class Meta:
+        model = User
+        fields = ('email', 'profile_image', 'nickname', 'like_genres')
+        widgets={
+            'nickname': forms.Textarea(attrs={"rows":"1", "cols":"6"}),
+        }
+
+    def clean_password(self):
+        return self.initial["password"]
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,137 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from movies.models import Movie
+# from django.db import models
+# from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser,AbstractUser)
+# from movies.models import Genre
 
-# Create your models here.
+# # Create your models here.
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, profile_image, kakao_id, nickname, followings, like_genres, password):
+#         if not email:
+#             raise ValueError('Users must have an email address')
+
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             profile_image = profile_image,
+#             kakao_id = kakao_id,
+#             nickname = nickname,
+#             followings = followings,
+#             like_genres = like_genres,
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, profile_image, kakao_id, nickname, followings, like_genres, password):
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             profile_image = profile_image,
+#             kakao_id = kakao_id,
+#             nickname = nickname,
+#             followings = followings,
+#             like_genres = like_genres
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+
+
+# class User(AbstractUser):
+#     email = models.EmailField(
+#         verbose_name='email',
+#         max_length=255,
+#         unique=True,
+#     )
+#     profile_image = models.ImageField()
+#     kakao_id = models.TextField(null=True)
+#     nickname = models.TextField()
+#     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+#     like_genres = models.ManyToManyField(Genre, symmetrical=False, related_name='like_users')
+
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+
+#     objects = UserManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['profile_image', 'nickname', 'followings','like_genres']
+
+#     def __str__(self):
+#         return self.email
+
+#     def has_perm(self, perm, obj=None):
+#         return True
+
+#     def has_module_perms(self, app_label):
+#         return True
+
+#     @property
+#     def is_staff(self):
+#         return self.is_admin
+
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractUser)
+from movies.models import Genre
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, profile_image, kakao_id, nickname, followings, like_genres, password):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            profile_image=profile_image,
+            kakao_id=kakao_id,
+            nickname=nickname,
+            followings=followings,
+            like_genres=like_genres,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, profile_image, kakao_id, nickname, followings, like_genres, password):
+        user = self.create_user(
+            email,
+            password=password,
+            profile_image=profile_image,
+            kakao_id=kakao_id,
+            nickname=nickname,
+            followings=followings,
+            like_genres=like_genres
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    profile_image = models.ImageField()
+    kakao_id = models.TextField(null=True)
+    nickname = models.TextField()
+    username=models.TextField()
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    #좋아요한영화
-    # like_movies= models.ManyToManyField(Movie, symmetrical=False)
+    like_genres = models.ManyToManyField(Genre, symmetrical=False, related_name='like_users')
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['profile_image', 'nickname', 'followings', 'like_genres']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
